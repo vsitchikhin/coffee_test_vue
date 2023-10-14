7462396216a456c226be097ec8336fb337e4d6bf
Vladimir Sitchikhin <vsitchikhin@gmail.com>
2023-10-14 18:12:15 +0300

    feat(setup): Подключение пакетов
    * Подключение pinia
    * Подключение axios

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import App from './App.vue';
 
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { createPinia } from 'pinia';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
@@ -20,8 +24,11 @@ const vuetify = createVuetify({
   directives,
 });
 
+const pinia = createPinia();
 
 createApp(App)
   .use(vuetify)
   .use(router)
+  .use(pinia)
+  .use(VueAxios, axios)
   .mount('#app');
